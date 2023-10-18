36cc702b5128d49eae75de0ae5d9a0dfedbab21a
Masum Alam <101336554+alamkare7890@users.noreply.github.com>
2023-10-18 19:37:24 +0000

    changes made on 19.10.2023

--- backtobasic/backtobasic.js
+++ backtobasic/backtobasic.js
@@ -1,1 +1,16 @@
-console.log("me");
+const accountId = 785445;
+let accountEmail = "masum@gmail.com";
+var accountPassword = "123654";
+accountCity = "Kolkata";
+//accountId = 455678;
+let accountState;
+
+accountEmail = "aasim@gmail.com";
+accountPassword = "124578";
+accountCity = "Delhi";
+/*
+    Never Use var due to issue in block scope 
+*/
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
